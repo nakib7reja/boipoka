6fb6de72bc96083f56c98fef1e6dfbffaa282c8f
NokibReja <nakib7reja@gmail.com>
2025-12-02 01:43:27 +0600

    banner added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import bannerImg from '../../assets/books.jpg'
+
+const Banner = () => {
+    return (
+        <div className='flex justify-between p-10 m-5 bg-gray-100 rounded-2xl'>
+            <div className='w-[45%] my-auto'>
+                <h1 className='text-5xl font-bold'>Books to freshen up your bookshelf</h1>
+                <button className='btn bg-[#23BE0A] text-white my-5 px-5 py-2 rounded-l text-l'>View The List</button>
+            </div>
+            <div className='w-[50%]'>
+                <img className='rounded-3xl w-[90%] mx-auto' src={bannerImg} alt="Img not found" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
